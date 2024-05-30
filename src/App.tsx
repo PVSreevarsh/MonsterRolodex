2ef8acda386d315a1a1288306bb070b7a2aba9ae
Sreevarsh Vasista <vasistasreevarsh1@gmail.com>
2024-05-30 20:20:45 +0530

    cards compoent

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Component, ReactNode } from "react";
+import Cards from "./components/cards/cards.component";
 
 
 class App extends Component {
@@ -16,6 +17,7 @@ class App extends Component {
       <div>
         <h1>Welcome to stackblitz development on class component</h1>
         <p>Hi {(this.state as any).name}</p>
+        <Cards></Cards>
         <button onClick={()=>{
           this.setState(() => {return {name:"Vasista Sreevarsh"}}, ()=>{
             // return (<h2>Added</h2>).
---
+++ src/components/cards/cards.component.tsx
@@ -0,0 +1,13 @@
+import { Fragment } from "react/jsx-runtime";
+
+const Cards: any = (): any=>{
+
+    return (
+        <Fragment>
+        <h1>Hello this is card component</h1>
+        </Fragment>
+    )
+
+}
+
+export default Cards;
